dc533f6a3a0b52bcba8ac97c0facd8e010b3a14e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 06:55:39 +0000

    Adding numeric_owner as keyword arguement

--- tools/datasets/sparse_dnn.py
+++ tools/datasets/sparse_dnn.py
@@ -138,7 +138,7 @@ class SparseDNN(object):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=dst)
